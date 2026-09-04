b070a139f5a5da7bea2260264c001e15e636ea26
YourShubbuu <dmnff5bmtj@privaterelay.appleid.com>
2026-09-04 15:48:47 +0530

    feat: add account sign in page

---
+++ apps/web/app/account/page.tsx
@@ -0,0 +1,6 @@
+import { ReaderHeader } from '@/components/reader-shell';
+import { AuthPanel } from '@/components/auth-panel';
+
+export default function AccountPage() {
+  return <><ReaderHeader /><AuthPanel /></>;
+}
